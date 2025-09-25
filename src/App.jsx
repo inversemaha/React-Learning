85a528e5fc166b110dce2627c552d1c5899394fc
inversemaha <mahajur88@gmail.com>
2025-09-26 05:50:38 +0600

    First react project created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Maha from "./maha";
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold pl-4">Hello World</h1>
+        <p className="mt-4 pl-4">
+          This is a simple Vite + React app with Tailwind CSS.
+        </p>
+      </div>
+      <Maha />
+    </>
+  );
+}
+
+export default App;
---
+++ src/maha.jsx
@@ -0,0 +1,9 @@
+function Maha() {
+  return (
+    <h1 className="text-2xl font-semibold text-green-500 content-center bg-yellow-300 pl-4">
+      This is my first component
+    </h1>
+  );
+}
+
+export default Maha;
